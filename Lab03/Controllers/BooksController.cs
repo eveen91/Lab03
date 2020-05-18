8dd63de82d025aff45af6f8b60003763750e0b91
Seba <eveen91@gmail.com>
2020-05-18 23:00:20 +0200

    userControler, bookControler

--- Lab03/Controllers/BooksController.cs
+++ Lab03/Controllers/BooksController.cs
@@ -14,8 +14,32 @@ namespace Lab03.Controllers
     {
         Books BooksList = new Books();
         //post
-        public void AddBook()
+        public IActionResult AddBook([FromBody] Book GivenBook)
         {
+            if (GivenBook.Author != "" && GivenBook.ID != 0 && GivenBook.Title != "")
+            {
+                if (BooksList.books.Find(x => x.ID == GivenBook.ID) == null)
+                {
+                    try
+                    {
+                        BooksList.books.Add(GivenBook);
+                        BooksList.SaveData();
+                        return StatusCode(200, "Ok");
+                    }
+                    catch
+                    {
+                        return StatusCode(400, "Bad Request");
+                    }
+                }
+                else
+                {
+                    return StatusCode(409, "Conflict, user with this id exist");
+                }
+            }
+            else
+            {
+                return StatusCode(400, "Bad Request");
+            }
         }
 
         //get id
@@ -34,14 +58,36 @@ namespace Lab03.Controllers
         }
 
         //post
-        public void UpdateBookById()
+        [HttpPut]
+        public IActionResult UpdateBookById([FromBody] Book GivenBook)
         {
-
+            var book = BooksList.books.Find(x => x.ID == GivenBook.ID);
+            if (book == null)
+            {
+                return StatusCode(400, "Bad Request");
+            }
+            else
+            {
+                BooksList.books[BooksList.books.FindIndex(x => x.ID == GivenBook.ID)] = GivenBook;
+                BooksList.SaveData();
+                return StatusCode(200, "ok");
+            }
         }
-        //?post
-        public void DeleteBookById()
-        {
 
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteBookById(string id)
+        {
+            var book = BooksList.books.Find(x => x.ID == int.Parse(id));
+            if (book == null)
+            {
+                return StatusCode(400, "nope");
+            }
+            else
+            {
+                BooksList.books.Remove(book);
+                BooksList.SaveData();
+                return StatusCode(200, "ok");
+            }
         }
     }
 }
--- Lab03/Controllers/UsersController.cs
+++ Lab03/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab03.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,40 +12,86 @@ namespace Lab03.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        Users users = new Users();
+        Users UserList = new Users();
 
         //post
         [HttpPost]
-        public void AddUser()
+        public IActionResult AddUser([FromBody] User GivenUser)
         {
+            //not null
+            if (GivenUser.EMail != "" && GivenUser.ID != 0 && GivenUser.Name != "" && GivenUser.Surname != "")
+            {
+                if (UserList.users.Find(x => x.ID == GivenUser.ID) == null)
+                {
+                    try
+                    {
+                        UserList.users.Add(GivenUser);
+                        UserList.SaveData();
+                        return StatusCode(200, "Ok");
+                    }
+                    catch
+                    {
+                        return StatusCode(400, "Bad Request");
+                    }
+                }
+                else
+                {
+                    return StatusCode(409, "Conflict, user with this id exist");
+                }
+            }
+            else
+            {
+                return StatusCode(400, "Bad Request");
+            }
         }
 
         //get id
         [HttpGet("{Id}")]
-        public string GetUserById(string Id)
+        public User GetUserById(string Id)
         {
-            string user = "";
-            return user;
+            var query = from user in UserList.users where user.ID.Equals(int.Parse(Id)) select user;
+            return query.First();
         }
 
         //get
         [HttpGet]
-        public List<string> GetAllUsers()
+        public List<User> GetAllUsers()
         {
-            return new List<string> { "User1", "User2" };
+            return UserList.users;
         }
 
-        //post
-        [HttpPost]
-        public void UpdateUserById()
-        {
 
+        [HttpPut]
+        public IActionResult UpdateUserById([FromBody] User GivenUser)
+        {
+            var user = UserList.users.Find(x => x.ID == GivenUser.ID);
+            if (user == null)
+            {
+                return StatusCode(400, "Bad Request");
+            }
+            else
+            {
+                UserList.users[UserList.users.FindIndex(x => x.ID == GivenUser.ID)] = GivenUser;
+                UserList.SaveData();
+                return StatusCode(200, "ok");
+            }
         }
 
-        //?post
-        public void DeleteUserById()
+        //?delete
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteUserById(string id)
         {
-
+            var user = UserList.users.Find(x => x.ID == int.Parse(id));
+            if (user == null)
+            {
+                return StatusCode(400, "nope");
+            }
+            else
+            {
+                UserList.users.Remove(user);
+                UserList.SaveData();
+                return StatusCode(200, "ok");
+            }
         }
     }
 }
